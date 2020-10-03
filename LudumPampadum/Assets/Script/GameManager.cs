4ab2c23b89f3a9b3ace30e70c2c7affa65223cd4
Yekonori <yannisdemacedo@gmail.com>
2020-10-03 10:29:22 +0200

    Player TP on click and scene can be reload with button

--- LudumPampadum/Assets/Script/GameManager.cs
+++ LudumPampadum/Assets/Script/GameManager.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Script Parameters
+
+    [Header("Main Canvas")]
+
+    [SerializeField] private bool onDebugMode;
+    [SerializeField] private Button reloadButton;
+
+    #endregion
+
+    #region Fields
+
+    #endregion
+
+    #region Unity Methods
+
+    private void Start()
     {
-        
+        if (onDebugMode)
+        {
+            reloadButton.gameObject.SetActive(true);
+            reloadButton.onClick.AddListener(ReloadScene);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    #endregion
+
+    private void ReloadScene()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
---
+++ LudumPampadum/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Script Parameters
+
+    public float distance = 4.5f;
+
+    #endregion
+
+    #region Fields
+
+    private List<Vector3> positions;
+
+    #endregion
+
+    #region Unity Methods
+
+    private void Start()
+    {
+        positions = new List<Vector3>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CastRayWorld();
+        }
+    }
+
+    #endregion
+
+    public void CastRayWorld()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 point = ray.origin + (ray.direction * distance);
+        //Debug.Log("World point " + point);
+
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+        if (hit.rigidbody != null)
+        {
+            positions.Add(new Vector3(hit.point.x, 0.1f, hit.point.z));
+        }
+
+        transform.position = positions[positions.Count - 1];
+    }
+}
