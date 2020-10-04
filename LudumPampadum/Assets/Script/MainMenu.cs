3a74687c4474a9d7fa177738493760a17f1a3e41
Alexis <alexis.sainsard@gmail.com>
2020-10-04 10:35:33 +0200

    Creation d'un main menu

---
+++ LudumPampadum/Assets/Script/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainPanel;
+    public GameObject selectLevel;
+    public GameObject creditPanel;
+
+    public void DiplayMain()
+    {
+        mainPanel.SetActive(true);
+        selectLevel.SetActive(false);
+        creditPanel.SetActive(false);
+    }
+
+    public void DisplaySelectionLevel()
+    {
+        mainPanel.SetActive(false);
+        selectLevel.SetActive(true);
+        creditPanel.SetActive(false);
+    }
+
+    public void DisplayCredit()
+    {
+        mainPanel.SetActive(false);
+        selectLevel.SetActive(false);
+        creditPanel.SetActive(true);
+    }
+
+    public void StartGame(int sceneIndex)
+    {
+        Debug.Log("Starting level " + sceneIndex);
+        //SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit()
+#endif
+    }
+}
+
